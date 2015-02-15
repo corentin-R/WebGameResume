74c7a660ab2bc1c0d2c2e0d9ecec33a979fe68ef
corentin-R <corentinraoult@laposte.net>
2015-02-14 21:23:41 -0500

    bug script

--- dlPhaser.sh
+++ dlPhaser.sh
@@ -4,5 +4,7 @@
 wget https://github.com/photonstorm/phaser/releases/download/v2.2.2/phaser.min.js
 
 #mv to lib dir
-mkdir /src/lib
-mv phaser.min.js ../src/lib/
+cd src/
+mkdir lib
+cd ..
+mv phaser.min.js src/lib
